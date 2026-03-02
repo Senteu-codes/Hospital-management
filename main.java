83d1064a83c81b3a9441177246ecaf086b5e857e
Senteu-codes <davidgotyou0@gmail.com>
2026-03-02 08:56:43 +0300

    Update doctor name in main class

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class  main {
     public static void main(String[] args) {
-        doctor doc1 = new doctor(1, "Dr. Smith", 1234567890, "drsmith@hospital.com", "123 Main St", 101, "Cardiology", "LIC12345");
+        doctor doc1 = new doctor(1, "Dr. Jared", 1234567890, "drjared@hospital.com", "123 Main St", 101, "Cardiology", "LIC12345");
         doc1.displayInfo();
 
         patient pat1 = new patient(2, "John Doe", 987654321, "johndoe@hospital.com", "456 Oak Ave", 201, "Diabetes", "Insurex");
@@ -11,5 +11,6 @@ public class  main {
 
 
     }
+
 
 }
